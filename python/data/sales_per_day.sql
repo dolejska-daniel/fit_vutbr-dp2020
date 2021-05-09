fb8adc0e8d727fbb68844e8d995df13766d5293d
Daniel Dolejška <dolejska-daniel@users.noreply.github.com>
2021-05-09 23:25:29 +0200

    Fix/Improve sales_per_day query

--- python/data/sales_per_day.sql
+++ python/data/sales_per_day.sql
@@ -1,5 +1,5 @@
-SELECT TIME_BUCKET('1d', s.created_at) AS "time",
-       SUM(s.sales_delta) AS "Sales"
+SELECT DATE(TIME_BUCKET('1d', s.created_at)) AS "time",
+       SUM(s.sales_delta)                    AS "Sales"
 FROM product__stats s
 WHERE created_at BETWEEN '2021-03-01 12:00' AND NOW()
 GROUP BY 1
