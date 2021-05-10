0f3a9b63c2615b23b41d71e0c5fea72b1f0feaf7
Daniel Dolejška <dolejska-daniel@users.noreply.github.com>
2021-05-10 14:18:23 +0200

    Cleaned sales_by_time_of_day SQL query

--- python/data/sales_by_time_of_day.sql
+++ python/data/sales_by_time_of_day.sql
@@ -1,14 +1,12 @@
 SELECT (CASE WHEN tmp.hours = 24 THEN 0 ELSE tmp.hours END) AS hours,
        tmp.sales
 FROM (
-   SELECT
-     WIDTH_BUCKET(EXTRACT(HOURS FROM created_at), 0, 24, 24) AS hours,
-     --WIDTH_BUCKET(EXTRACT(MINUTES FROM created_at), 0, 60, 60) AS minutes,
-     SUM(sales_delta) AS "sales"
+   SELECT WIDTH_BUCKET(EXTRACT(HOURS FROM created_at), 0, 24, 24) AS hours,
+          SUM(sales_delta)                                        AS sales
    FROM product__stats ps
    WHERE ps.created_at > '2021-03-30 00:00'
        AND ps.created_at <= '2021-04-12 23:59'
        AND ps.sales_delta <= 3
-   GROUP BY hours  --, minutes
+   GROUP BY hours
 ) tmp
-ORDER BY hours
+ORDER BY hours;
